8a05190ad302fb04b6f625c14020b0ff57eb7718
Dave Simel <davesimel@gmail.com>
2015-06-05 12:09:37 -0400

    Added series name form and inserted into database

---
+++ models/series.rb
@@ -0,0 +1,8 @@
+class Series
+
+  
+
+
+
+
+end
--- server.rb
+++ server.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'sinatra/flash'
-require 'pry'
 require 'pg'
+require 'pry'
+
+require_relative 'models/artwork'
+require_relative 'models/series'
 
 def db_connection
   begin
-    connection = PG.connect(dbname: "groceries")
+    connection = PG.connect(dbname: "artinventory")
     yield(connection)
   ensure
     connection.close
@@ -16,6 +19,15 @@ get "/" do
   erb :index
 end
 
-post "/art" do
+get "/series" do
+  erb :series
+end
 
+post "/series" do
+  series_name = params['series_name']
+
+  db_connection do |conn|
+    conn.exec_params("INSERT INTO series(name) VALUES($1)", [series_name])
+  end
+  redirect "/series"
 end
